fde26c53b3f7789dc0f9cf0e528754c4c9fd4a83
Oikkonen <matheusteixeiradossantos2000@gmail.com>
2023-03-19 00:43:33 -0300

    teste

--- teste_login1/App.js
+++ teste_login1/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen options= {{headerShown: false}} name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- teste_login1/screens/RegisterScreen.js
+++ teste_login1/screens/RegisterScreen.js
@@ -5,35 +5,21 @@ import { useNavigation } from '@react-navigation/core'
 
 
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const navigation = useNavigation();
 
-  
-  
-  const handleLogin = () => {
+  const handleSignup = () => {
     auth
-        .signInWithEmailAndPassword(email, password)
+        .createUserWithEmailAndPassword(email, password)
         .then(userCredentials => {
             const user = userCredentials.user;
-            console.log('Logged in with: ', user.email);
+            console.log('Registered in with: ',user.email);
         })
         .catch(error => alert(error.message))
   }
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-        if (user) {
-            navigation.replace("Home")
-        }
-    })
-    return unsubscribe
-  }, [])
 
-  const toRegister = () => {
-    navigation.push("Register")
-  }
-  
   return (
     <KeyboardAvoidingView
     style={styles.container}
@@ -53,77 +39,82 @@ const LoginScreen = () => {
             style = {styles.input}
             secureTextEntry
         />
+       
       </View>
 
       <View style = {styles.buttonContainer}>
-      <TouchableOpacity
-            onPress={handleLogin}
-            style = {styles.button}
-        >
-            <Text style = {styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-
+        
         <TouchableOpacity
-            onPress={toRegister}
+           onPress={handleSignup}
             style = {[styles.button, styles.buttonOutline]}
         >
             <Text style = {styles.buttonOutlineText}>Registre-se</Text>
         </TouchableOpacity>
+
+        
 
       </View>
     </KeyboardAvoidingView>
   )
 }
 
-export default LoginScreen
+export default RegisterScreen
 
 const styles = StyleSheet.create({
 container: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
+  flex: 2,
+  justifyContent: 'center',
+  alignItems: 'center',
 },
 inputContainer: {
-    width: '80%'
+  width: '80%',
 },
 input: {
-    backgroundColor: 'white',
-    paddingHorizontal: 75,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-    
-    
+  backgroundColor: 'white',
+  paddingHorizontal: 40,
+  paddingVertical: 10,
+  borderRadius: 10,
+  marginTop: 10,
+  textAlign: 'center'
+  
+  
 },
 buttonContainer: {
-    width: '60%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
+  width: '60%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: 40,
 },
 button: {
-    backgroundColor: '#0782F9',
-    width: '100%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center'
+  backgroundColor: '#0782F9',
+  width: '100%',
+  padding: 15,
+  borderRadius: 10,
+  alignItems: 'center'
+},
+buttonBack: {
+  backgroundColor: '#0782F9',
+  width: '100%',
+  padding: 25,
+  borderRadius: 10,
+  alignItems: 'center'
 },
 buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#0782F9',
-    borderWidth: 2,
+  backgroundColor: 'white',
+  marginTop: 5,
+  borderColor: '#0782F9',
+  borderWidth: 2,
 },
 buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16
+  color: 'white',
+  fontWeight: '700',
+  fontSize: 16
 },
 
 buttonOutlineText: {
-    color: '#0782F9',
-    fontWeight: '700',
-    fontSize: 16
+  color: '#0782F9',
+  fontWeight: '700',
+  fontSize: 16
 },
 
 })
